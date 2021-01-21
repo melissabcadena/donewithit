0954c091e209ad3032aec64baf494141f33ebc9f
Melissa Cadena <melissabcadena@gmail.com>
2021-01-21 15:26:44 -0600

    finish welcome screen

--- App.js
+++ App.js
@@ -3,17 +3,7 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   
-  const { landscape } = useDeviceOrientation();
-
   return (
     <WelcomeScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-  },
-});
